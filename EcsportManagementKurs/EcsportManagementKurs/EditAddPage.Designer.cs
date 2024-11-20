c5db2aa77f4f25468bec7d4aa439126eec598884
ZakharFilip <157304145+ZakharFilip@users.noreply.github.com>
2024-11-20 14:59:38 +0300

    New Cursach URAAAA

--- EcsportManagementKurs/EcsportManagementKurs/EditAddPage.Designer.cs
+++ EcsportManagementKurs/EcsportManagementKurs/EditAddPage.Designer.cs
@@ -110,6 +110,7 @@
             this.Controls.Add(this.groupBox1);
             this.Name = "EditAddPage";
             this.Text = "Редактировать договор договор";
+            this.Load += new System.EventHandler(this.EditAddPage_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
--- EcsportManagementKurs/EcsportManagementKurs/EditAddPage.cs
+++ EcsportManagementKurs/EcsportManagementKurs/EditAddPage.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace EcsportManagementKurs
 {
@@ -45,11 +46,69 @@ namespace EcsportManagementKurs
         private void changeItemButton_Click(object sender, EventArgs e)
         {
             //кнопка для редактирования контракта 
-            //Добавить код для изменения коетракта
+            
 
+            InitializeComponent();
+            string connectionString = @"Data Source=pcsqlstud01;Initial Catalog=10220468;Integrated Security=True;Encrypt=False";
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+            
+
+            MessageBox.Show(itemId.ToString());
+
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                //Добавить код для изменения коетракта
+                
+                string querySupplier = "UPDATE Contract SET idSupplier = @updatedValue WHERE idDocument = itemId";
+                string queryRecipient = "UPDATE Contract SET idRecipient = @updatedRecipient WHERE idDocument = itemId";
+                string queryData = "UPDATE Contract SET idSupplier = @updatedData WHERE idDocument = itemId";
+
+
+                int recordIdentifier =itemId;
+                string newSupplierString = textBox4.Text;
+                string newRecipient = textBox2.Text;
+                string newData = textBox1.Text;
+
+
+
+                SqlCommand commandAddSupplier = new SqlCommand(querySupplier, connection);
+                commandAddSupplier.Parameters.AddWithValue("@updatedValue", newSupplierString);
+
+                SqlCommand commandAddRecipient = new SqlCommand(queryRecipient, connection);
+                commandAddRecipient.Parameters.AddWithValue("@updatedRecipient", newRecipient);
+
+                SqlCommand commandAddData = new SqlCommand(queryData, connection);
+                commandAddRecipient.Parameters.AddWithValue("@updatedData", newData);
+
+
+                try
+                {
+                    
+                    int rowsAffected = commandAddSupplier.ExecuteNonQuery(); // Выполняем запрос
+
+                    // Проверяем, были ли затронуты строки
+                    if (rowsAffected > 0)
+                    {
+                        MessageBox.Show("Данные обновлены успешно.");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Не удалось обновить данные. Проверьте ID.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Произошла ошибка: " + ex.Message);
+                }
+
+            }
         }
 
+        private void EditAddPage_Load(object sender, EventArgs e)
+        {
 
+        }
     }
 }
 
